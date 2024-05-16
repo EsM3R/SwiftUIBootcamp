7851090685adbd5aac5bd7ead2caaf1778eea9d3
Mehmet Vural <105605335+EsM3R@users.noreply.github.com>
2024-05-16 07:57:12 +0300

    Initial Commit

---
+++ SwiftUIBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBootcamp
+//
+//  Created by Mehmet Vural on 16.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Mehmet Vural on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
